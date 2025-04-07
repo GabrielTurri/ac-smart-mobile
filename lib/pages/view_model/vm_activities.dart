d1ca8635d79db8087b0fc70d084ef9674574840e
Gabriel Turri <ga-briel.t.a@hotmail.com>
2025-04-07 17:10:23 -0300

    criada viwemodel para filtrar as atividade

--- lib/pages/view_model/vm_activities.dart
+++ lib/pages/view_model/vm_activities.dart
@@ -1,1 +1,7 @@
+import 'package:ac_smart/models/activity.dart';
+import 'package:ac_smart/pages/ui/activity_container.dart';
 
+consultarAtividadeContainer(id) {
+  // filtro de reprovado ou não
+  if (atividades[id]) return ActivityContainer(id);
+}
